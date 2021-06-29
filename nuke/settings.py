4e16e5880ec3c697db44b7c088aa9f59c2d34109
Vikash Patel <vikaspatelp83@gmail.com>
2021-06-29 15:29:39 +0530

    Create browsable api of users

--- nuke/settings.py
+++ nuke/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # applications
+    'rest_framework',
     'apis'
 ]
 
@@ -84,6 +85,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
